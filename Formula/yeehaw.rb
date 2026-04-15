fc74f72730fa66ba9d6c86d07b9a5d3dc1f3de5c
Cameron (Air) <cdudley4@gmail.com>
2026-04-15 11:43:39 -0700

    yeehaw 0.1.3

--- Formula/yeehaw.rb
+++ Formula/yeehaw.rb
@@ -3,17 +3,17 @@
 class Yeehaw < Formula
   desc "Terminal ranch management for projects, servers, and Claude sessions"
   homepage "https://yeehaw.cool"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Colmbus72/yeehaw/releases/download/v#{version}/yeehaw-aarch64-apple-darwin.tar.xz"
-      sha256 "b37d1ef64ec0ad5b9ec1e2f41508164b50baf05188f049358f25878e1f63f870"
+      sha256 "af48551c6cca3bf2db1d800d417a143f9bc7399b5f9b3c5ce4566bc4cf6e9027"
     end
     on_intel do
       url "https://github.com/Colmbus72/yeehaw/releases/download/v#{version}/yeehaw-x86_64-apple-darwin.tar.xz"
-      sha256 "20b1589b2b16e7a7167f220c40205fd8aaccd5c5b0ecc64537f5449fef9fd54f"
+      sha256 "a5652c9fdfb88a8a7b1d9ab3d9a3cc81a131ddf09e0e1e4d114193bc8f6ce49d"
     end
   end
 
